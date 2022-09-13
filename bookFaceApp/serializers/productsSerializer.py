e9c3d834c77eaf01f8d3901327ec8d10b453df30
Michael Jimenez <ingmichaeljimenez@gmail.com>
2022-09-13 09:12:43 -0500

    Update productsSerializer.py

--- bookFaceApp/serializers/productsSerializer.py
+++ bookFaceApp/serializers/productsSerializer.py
@@ -1,8 +1,7 @@
-from rest_framework import serializers
 from bookFaceApp.models.products import products
+from rest_framework import serializers
 
-class productsSerializer(serializers.ModelSerializer):
-  class Meta():
-      model=products
-      fields=['idproducts','name_prod','author','editorial','category','tipoProducto','num_page','isbn','state','rank','formato','presentation','image','price','visits','description','cantidadExistencia','ventas']
-   
+class productoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = products
+        fields = ['idproduct']
