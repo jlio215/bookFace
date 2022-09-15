e51e40b709acd27335c316e29fa168d4b1e5b163
Michael Jimenez <ingmichaeljimenez@gmail.com>
2022-09-14 22:20:05 -0500

    Update products.py and productsSerializer.py

--- bookFaceApp/models/products.py
+++ bookFaceApp/models/products.py
@@ -1,8 +1,9 @@
 from django.db import models
 #from .kindproduct import nombretipodeproducto
 from .inventario import inventory
-from .tipoproducto import product_type
+#from .tipoproducto import product_type
 from .ventas import ventas
+from .user import User
 
 class products(models.Model):
     idproducts = models.AutoField(primary_key=True)
@@ -15,15 +16,16 @@ class products(models.Model):
     isbn= models.CharField(max_length=45)
     state= models.CharField(max_length=250)
     rank= models.CharField(max_length=45)
-    formato= models.CharField(max_length=45)
+    format_prod= models.CharField(max_length=45)
     presentation= models.CharField(max_length=45)
     image= models.CharField(max_length=60)
     price= models.DecimalField(max_digits=6, decimal_places=3)
     visits= models.BigIntegerField()
     description=models.CharField(max_length=250) 
-    tipoProducto = models.ForeignKey(product_type, related_name='tipo_producto', on_delete=models.CASCADE)
-    cantidadExistencia = models.ForeignKey(inventory, related_name='existencias', on_delete=models.CASCADE)    
-    ventas = models.ForeignKey(ventas, related_name='ventas', on_delete=models.CASCADE)
-    #products = models.ForeignKey(products_has_categories, related_name='idproducts', on_delete=models.CASCADE)
-    #idAccount = models.ForeignKey(User, related_name='account', on_delete=models.CASCADE)
-    #idAccount = models.ForeignKey(User, related_name='account', on_delete=models.CASCADE)
+    name_cat=models.CharField(max_length=45)
+    subcategory=models.CharField(max_length=45)
+    amount = models.ForeignKey(inventory, related_name='existencias', on_delete=models.CASCADE)    
+    stock_prod=models.ForeignKey(ventas, related_name='ventas', on_delete=models.CASCADE)
+    user=models.ForeignKey(User, related_name='usuaario', on_delete=models.CASCADE) 
+    #tipoProducto = models.ForeignKey(product_type, related_name='tipo_producto', on_delete=models.CASCADE)
+    #ventas = models.ForeignKey(ventas, related_name='ventas', on_delete=models.CASCADE)
--- bookFaceApp/serializers/productsSerializer.py
+++ bookFaceApp/serializers/productsSerializer.py
@@ -4,4 +4,26 @@ from rest_framework import serializers
 class productoSerializer(serializers.ModelSerializer):
     class Meta:
         model = products
-        fields = ['idproducts','name_prod','author','editorial','category','tipoProducto','num_page','isbn','state','rank','formato','presentation','image','price','visits','description','cantidadExistencia','ventas']
+        fields = ['idproducts','name_prod','author','editorial','category','type_prod',
+                  'num_page','isbn','state','rank','format_prod','presentation','image','price',
+                  'visits','description','name_cat','subcategory','amount','stock_prod','user']
+        
+        
+        
+      
+        
+        
+        
+       
+        
+        
+        
+        
+   
+        
+        
+        
+        
+        
+        
+  
